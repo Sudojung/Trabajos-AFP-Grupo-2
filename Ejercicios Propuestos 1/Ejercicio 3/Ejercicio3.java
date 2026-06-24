50f5e68a4f9723425356f7983ff47e155b17ed1b
Renato Dario Salcedo Gaviria <renato.salcedog@unmsm.edu.pe>
2026-06-24 08:36:20 -0500

    Add files via upload

---
+++ Ejercicios Propuestos 1/Ejercicio 3/Ejercicio3.java
@@ -0,0 +1,9 @@
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        int suma = 0;
+        for (int i = 1; i <= 20; i++) {
+            suma += (i * 3);
+        }
+        System.out.println("La suma de los 20 primeros múltiplos de 3 es: " + suma);
+    }
+}
