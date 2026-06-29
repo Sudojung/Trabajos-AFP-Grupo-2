261b165d13f592a09da3b907f4a2e00843383254
Ana de Gabriela <anadegabrielaa@gmail.com>
2026-06-29 08:33:19 -0500

    Create Ejercicio 2.jl

---
+++ Ejercicios Propuestos 3/Ejercicio 2/Ejercicio 2.jl
@@ -0,0 +1,26 @@
+Algoritmo PromedioDeNotas
+    Definir cantidad, i Como Entero
+    Definir suma, promedio Como Real 
+    Definir vectorNotas Como Real
+    
+    suma <- 0
+
+    Escribir "Ingrese la cantidad de notas a registrar:"
+    Leer cantidad
+    
+    Dimension vectorNotas[cantidad]
+    
+    Escribir "Ingrese las notas una por una:"
+    
+    Para i <- 0 Hasta cantidad - 1 Con Paso 1 Hacer
+        Escribir "Ingrese la nota para la posición ", i, ":"
+        Leer vectorNotas[i]
+        
+        suma <- suma + vectorNotas[i]
+    Fin Para
+    
+    promedio <- suma / cantidad
+    
+    Escribir "La suma total es: ", suma
+    Escribir "El promedio de las notas es: ", promedio
+FinAlgoritmo
