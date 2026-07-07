c36b326b62446ca83350b7cda949aa6fd04baa62
Fr_David <franco.zevallossi@gmail.com>
2026-07-07 02:24:13 -0500

    Add files via upload

---
+++ Ejercicios Propuestos 3/Ejercicio 14/ejercicio14.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void ordenarVector(int v[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (v[j] > v[j + 1]) {
+                int temp = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void imprimirVector(int v[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Ingrese la cantidad de elementos: ";
+    cin >> n;
+
+    int v[100];
+    for (int i = 0; i < n; i++) {
+        cout << "Elemento " << i << ": ";
+        cin >> v[i];
+    }
+
+    ordenarVector(v, n);
+    cout << "Vector ordenado: ";
+    imprimirVector(v, n);
+
+    return 0;
+}
